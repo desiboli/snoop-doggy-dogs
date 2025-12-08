b6bb98800f09dc96a877840480cd3bf7af989e04
desiboli <adam.benals@gmail.com>
2025-12-08 10:43:27 +0100

    dog list cleanup

--- src/components/dog-list.tsx
+++ src/components/dog-list.tsx
@@ -30,25 +30,18 @@ const DogList = ({ breed }: DogListProps) => {
   if (isLoading || isLoadingByBreed) return <div>Loading dogs...</div>
   if (isError || isErrorByBreed) return <div>Error loading dogs</div>
 
+  const allDogs = breed === "" ? dogs : dogsByBreed
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {breed === ""
-        ? dogs?.map((dog, index) => (
-            <DogCard
-              key={index}
-              name={dog.name}
-              photo={dog.photo}
-              breed={dog.breed}
-            />
-          ))
-        : dogsByBreed?.map((dog, index) => (
-            <DogCard
-              key={index}
-              name={dog.name}
-              photo={dog.photo}
-              breed={dog.breed}
-            />
-          ))}
+      {allDogs?.map((dog, index) => (
+        <DogCard
+          key={index}
+          name={dog.name}
+          photo={dog.photo}
+          breed={dog.breed}
+        />
+      ))}
     </div>
   )
 }
