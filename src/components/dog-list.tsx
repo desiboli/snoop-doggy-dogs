dc3bad066ce2a6c197ab5bbc24f30252d89754e0
desiboli <adam.benals@gmail.com>
2025-12-08 10:54:45 +0100

    show more dogs button

--- src/components/dog-list.tsx
+++ src/components/dog-list.tsx
@@ -1,11 +1,16 @@
 import { useFetchDogsByBreedQuery, useFetchDogsQuery } from "@/store/dogs"
 import DogCard from "./dog-card"
+import { useState } from "react"
+import { Button } from "./ui/button"
+import { MoveDownIcon } from "lucide-react"
 
 interface DogListProps {
   breed: string
 }
 
 const DogList = ({ breed }: DogListProps) => {
+  const [visibleCount, setVisibleCount] = useState(6)
+
   const {
     data: dogs,
     isLoading,
@@ -31,17 +36,30 @@ const DogList = ({ breed }: DogListProps) => {
   if (isError || isErrorByBreed) return <div>Error loading dogs</div>
 
   const allDogs = breed === "" ? dogs : dogsByBreed
+  const displayedDogs = allDogs?.slice(0, visibleCount)
+  const hasMore = allDogs ? visibleCount < allDogs.length : false
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {allDogs?.map((dog, index) => (
-        <DogCard
-          key={index}
-          name={dog.name}
-          photo={dog.photo}
-          breed={dog.breed}
-        />
-      ))}
+    <div className="space-y-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {displayedDogs?.map((dog, index) => (
+          <DogCard
+            key={index}
+            name={dog.name}
+            photo={dog.photo}
+            breed={dog.breed}
+          />
+        ))}
+      </div>
+
+      {hasMore && (
+        <div className="flex justify-center">
+          <Button onClick={() => setVisibleCount((prev) => prev + 6)}>
+            Show more
+            <MoveDownIcon className="w-4 h-4" />
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
